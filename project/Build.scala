86823526be50252958e3243d0cb712eb9e1a11b4
Marc CARRÉ <carre.marc@gmail.com>
2013-01-13 19:06:54 +0000

    Initial configuration for Servlet 2.5 containers

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+import com.github.play2war.plugin._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "test"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here  
+    Play2WarKeys.servletVersion := "2.5"    
+  ).settings(Play2WarPlugin.play2WarSettings: _*)
+
+}
